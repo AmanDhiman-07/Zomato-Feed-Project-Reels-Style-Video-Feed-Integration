1217605793b3450832ae42db6bab5cf6a8cd5955
name <dhimanamandeep09@gmail.com>
2025-10-25 20:23:22 +0530

    Remove frontend code from GitHub

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-// import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./App.css"; // or "./App.css"
-
-createRoot(document.getElementById("root")).render(<App />);
